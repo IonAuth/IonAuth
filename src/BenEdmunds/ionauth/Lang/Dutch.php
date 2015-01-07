31b7da44021b553f9b88d6feaf541dd2573f498a
Tim Joosten <Topairy@gmail.com>
2015-01-07 23:20:18 +0100

    Add language files
    - English
    - French
    - Dutch
    
    More languages later

---
+++ src/BenEdmunds/ionauth/Lang/Dutch.php
@@ -0,0 +1,72 @@
+<?php
+
+  /**
+   * ----------------------------------------------------
+   * Info:
+   * ----------------------------------------------------
+   * @author Tim Joosten
+   * @package Ion Auth 3
+   *
+   * Description: The dutch language file for Ion Auth 3
+   * ----------------------------------------------------
+   */
+
+   // Account creation
+   $accountCreationSuccesful           = 'Account is aangemaakt';
+   $accountCreationUnsuccesful         = 'Account aanmaken is mislukt';
+   $accountCreationDuplicateEmail      = 'E-mail adres is al in gebruik of ongeldig';
+   $accountCreationDuplicateUsername   = 'Gebruikersnaam is al in gebruik of ongeldig';
+   $accountCreationMissingDefaultGroup = 'Standdard groep is niet ingesteld';
+   $accountCreationInvalidDefaultGroup = 'Standaard groepsnaam is ongeldig';
+
+   // Password
+   $passwordChangeSuccessful           = 'Wachtwoord is succesvol gewijzigd';
+   $passwordChangeUnsuccessful         = 'Wachtwoord wijzigen is mislukt';
+   $forgotPasswordSuccessful           = 'E-mail om thet wachtwoord te resetten is verzonden';
+   $forgotPasswordUnsuccessful         = 'Wachtwoord resetten is mislukt';
+
+   // Activation
+   $activateSuccessful                 = 'Account is geactiveerd';
+   $activateUnsuccessful               = 'Account activeren is mislukt';
+   $deactivateSuccessful               = 'Account is gedeactiveerd';
+   $deactivateUnsuccessful             = 'Account deactiveren is mislukt';
+   $activationEmailSuccessful          = 'Activatie e-mail is verzonden';
+   $activationEmailUnsuccessful        = 'Activatie e-mail verzenden is mislukt';
+
+   // Login / Logout
+   $loginSuccessful                    = 'U bent ingelogd';
+   $loginUnsuccessful                  = 'Login is incorrect';
+   $loginUnsuccessfulNotActive         = 'Account is niet geactiveerd';
+   $loginTimeout                       = 'Account is tijdelijk geblokkeerd';
+   $logoutSuccessful                   = 'U bent succesvol uitgelogd';
+
+   // Account changes
+   $updateSuccessful                   = 'Account is bijgewerkt';
+   $updateUnsuccessful                 = 'Account bijwerken is mislukt';
+   $deleteSuccessful                   = 'Gebruiker is verwijderd';
+   $deleteUnsuccessful                 = 'Gebruiker verwijderen is mislukt';
+
+   // Groups
+   $groupCreationSuccessful            = 'U hebt succesvol een groep gecreerd';
+   $groupAlreadyExists                 = 'De groep bestaat al';
+   $groupUpdateSuccessful              = 'Groep is succesvol bijgewerkt';
+   $groupDeleteSuccessful              = 'Groep is verwijderd';
+   $groupDeleteUnsccessful             = 'Kon de groep niet verwijderen';
+   $groupNameRequired                  = 'Groep naam is vereist';
+
+   // Activation Email
+   $emailActivationSubject             = 'Activering account';
+   $emailActivateHeading               = 'Activering';
+   $emailActivateSubheading            = 'Activeer uw account';
+   $emailActivateLink                  = 'Hyperlink';
+
+   // Forgot Password Email
+   $emailForgottenPasswordSubject      = 'Niew wachtwoord';
+   $emailForgotPasswordHeading         = 'Wachtwoord terughalen';
+   $emailForgotPasswordSubheading      = 'Haal uw wachtwoord op';
+   $emailForgotPasswordLink            = 'Hyperlink';
+
+   // New Password Email
+   $emailNewPasswordSubject            = 'Verificatie verloren wachtwoord';
+   $emailNewPasswordHeading            = 'Nieuw wachtwoord';
+   $emailNewPasswordSubheading         = 'Account activering';
---
+++ src/BenEdmunds/ionauth/Lang/French.php
@@ -0,0 +1,72 @@
+<?php
+
+  /**
+   * ----------------------------------------------------
+   * Info:
+   * ----------------------------------------------------
+   * @author Tim Joosten
+   * @package Ion Auth 3
+   *
+   * Description: The French language file for Ion Auth 3
+   * ----------------------------------------------------
+   */
+
+   // Account creation
+   $accountCreationSuccesful           = 'Compte créé avec succés';
+   $accountCreationUnsuccesful         = 'Impossible de créer le compte';
+   $accountCreationDuplicateEmail      = 'Email déjà utilisé ou invalide';
+   $accountCreationDuplicateUsername   = 'Nom d\'utilisateur déjà utilisé ou invalide';
+   $accountCreationMissingDefaultGroup = 'Le groupe par défaut n\'est pas configuré';
+   $accountCreationInvalidDefaultGroup = 'Le nom du groupe par défaut n\'est pas valide';
+
+   // Password
+   $passwordChangeSuccessful           = 'Le mot de passe a été changé avec succès';
+   $passwordChangeUnsuccessful         = 'Impossible de changer le mot de passe';
+   $forgotPasswordSuccessful           = 'Mail de réinitialisation du mot de passe envoyé';
+   $forgotPasswordUnsuccessful         = 'Impossible de réinitialiser le mot de passe';
+
+   // Activation
+   $activateSuccessful                 = 'Compte activé';
+   $activateUnsuccessful               = 'Impossible d\'activer le compte';
+   $deactivateSuccessful               = 'Compte désactivé';
+   $deactivateUnsuccessful             = 'Impossible de désactiver le compte';
+   $activationEmailSuccessful          = 'Email d\'activation envoyé avec succès';
+   $activationEmailUnsuccessful        = 'Impossible d\'envoyer l\'email d\'activation';
+
+   // Login / Logout
+   $loginSuccessful                    = 'Connecté avec succès';
+   $loginUnsuccessful                  = 'Erreur lors de la connexion';
+   $loginUnsuccessfulNotActive         = 'Ce compte est inactif';
+   $loginTimeout                       = 'Compte temporairement verrouillé. Réessayez plus tard.';
+   $logoutSuccessful                   = 'Déconnexion effectuée avec succès';
+
+   // Account changes
+   $updateSuccessful                   = 'Compte utilisateur mis à jour avec succès';
+   $updateUnsuccessful                 = 'Impossible de mettre à jour le compte utilisateur';
+   $deleteSuccessful                   = 'Utilisateur supprimé';
+   $deleteUnsuccessful                 = 'Impossible de supprimer l\'utilisateur';
+
+   // Groups
+   $groupCreationSuccessful            = 'Groupe créé avec succès';
+   $groupAlreadyExists                 = 'Nom du groupe déjà pris';
+   $groupUpdateSuccessful              = 'Informations sur le groupe mis à jour';
+   $groupDeleteSuccessful              = 'Groupe supprimé';
+   $groupDeleteUnsccessful             = 'Impossible de supprimer le groupe';
+   $groupNameRequired                  = 'Le nom du groupe est un champ obligatoire';
+
+   // Activation Email
+   $emailActivationSubject             = 'Activation du compte';
+   $emailActivateHeading               = 'Activer le compte pour %s';
+   $emailActivateSubheading            = 'S\'il vous plaît cliquer sur ce lien pour %s.';
+   $emailActivateLink                  = 'Activez votre compte';
+
+   // Forgot Password Email
+   $emailForgottenPasswordSubject      = 'Mot de Passe Oublié - Vérification';
+   $emailForgotPasswordHeading         = 'Réinitialiser le mot de passe pour %s';
+   $emailForgotPasswordSubheading      = 'S\'il vous plaît cliquer sur ce lien pour %s.';
+   $emailForgotPasswordLink            = 'Réinitialiser votre mot de passe';
+
+   // New Password Email
+   $emailNewPasswordSubject            = 'Nouveau Mot de Passe';
+   $emailNewPasswordHeading            = 'Nouveau Mot de Passe pour %s';
+   $emailNewPasswordSubheading         = 'Votre mot de passe a été réinitialisé à : %s';
